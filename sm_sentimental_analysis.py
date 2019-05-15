e6a7ed089faec5fd7975adfd2fae5e7b8bc97aec
Hussam K, Rozn <h.kurd@rozn.org>
2019-05-15 16:14:50 +0300

    update the data and the classification performance

--- sm_sentimental_analysis.py
+++ sm_sentimental_analysis.py
@@ -13,6 +13,7 @@ from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC, LinearSVC
 
+from libs.vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from sm_settings import SMSettings
 
 class SmSentimentalAnalysis:
@@ -26,43 +27,44 @@ class SmSentimentalAnalysis:
     features = None
 
     def __init__(self):
-        n_estimators = 100
-
+        n_estimators = 10
         self.classifier = OneVsRestClassifier(BaggingClassifier(LinearSVC(penalty="l1", dual=False), max_samples=1.0 / n_estimators, n_estimators=n_estimators), n_jobs=-1)
-        self.features = self.get_features()
+        self.vectorizer = TfidfVectorizer(min_df = 5,
+                             max_df = 0.8,
+                             sublinear_tf = True,
+                             use_idf = True)
         self.set_train_test_data()
         self.set_classification_pipline()
 
-    def get_features(self):
-        vectorizer = {
-            'tfid': TfidfVectorizer(min_df = 5,
-                                 max_df = 0.8,
-                                 sublinear_tf = True,
-                                 use_idf = True),
-        }
-
-        return FeatureUnion([("tfid", vectorizer['tfid'])])
 
     def set_train_test_data(self):
 
         docs = pd.read_csv(SMSettings.datasets_root_path.format(SMSettings.default_lang) + SMSettings.training_dataset_name ,encoding="utf-8")
         msk = np.random.rand(len(docs["polarity"])) < 0.8
-        self.train_data = docs["text"][msk]
-        self.train_vectors = docs["polarity"][msk]
+        self.train_data_content = docs["text"][msk]
+        self.train_data_label = docs["polarity"][msk]
 
-        self.test_data = docs["text"][~msk]
-        self.test_vectors = docs["polarity"][~msk]
+        docs_test = pd.read_csv(SMSettings.datasets_root_path.format(SMSettings.default_lang) + SMSettings.test_dataset_name ,encoding="utf-8")
+        msk_test = np.random.rand(len(docs_test["polarity"])) < 0.2
+
+        self.test_data_content = docs_test["text"][msk_test]
+        self.test_data_label = docs_test["polarity"][msk_test]
+
+        self.train_vectors = self.vectorizer.fit_transform(self.train_data_content)
+        self.test_vectors = self.vectorizer.transform(self.test_data_content)
 
 
 
     def set_classification_pipline(self):
-        pipeline = Pipeline([
-                        ('features', self.features),
-                        ('classifier', self.classifier)])
-        pipeline.fit(self.train_data, self.train_vectors)
-        pred = pipeline.predict(self.test_data)
+        self.classifier.fit(self.train_vectors, self.train_data_label)
+        pred = self.classifier.predict(self.test_vectors)
+        analyser = SentimentIntensityAnalyzer()
 
-        report = classification_report(self.test_vectors, pred, output_dict=True)
+        for idx, input, prediction, label in zip(enumerate(self.test_data_content), self.test_data_content, pred, self.test_data_label):
+            print(analyser.polarity_scores(input))
+            print("No.", idx[0], 'input,',input, ', has been classified as', prediction, 'and should be', label)
+
+        report = classification_report(self.test_data_label, pred, output_dict=True)
         print(report)
 
 
--- sm_settings.py
+++ sm_settings.py
@@ -2,3 +2,4 @@ class SMSettings:
     default_lang = 'ar'
     datasets_root_path = 'trainingDataSets/{}/'
     training_dataset_name = 'data_warehouse_all.csv'
+    test_dataset_name = 'test_tweets_data_all.csv'
